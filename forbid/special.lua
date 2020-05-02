24bc7a9b369bba91aa84e1b67a6d631995a0c08d
purerosefallen <78877@qq.com>
2020-05-02 16:11:21 +0800

    typo

--- forbid/special.lua
+++ forbid/special.lua
@@ -7,7 +7,7 @@ function Auxiliary.PreloadUds()
 	e1:SetCode(EVENT_PREDRAW)
 	e1:SetCondition(function()
 		local turnc=Duel.GetTurnCount()
-		return tunc>1 and turnc<=10
+		return turnc>1 and turnc<=10
 	end)
 	e1:SetOperation(function()
 		local tp=Duel.GetTurnPlayer()
