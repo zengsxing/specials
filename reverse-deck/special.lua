aa117084f21f57289b55c13de160408ea4901b1f
purerosefallen <78877@qq.com>
2021-03-10 08:54:52 +0800

    one more draw for player who go next

--- reverse-deck/special.lua
+++ reverse-deck/special.lua
@@ -1,4 +1,8 @@
 --天変地異
+function inititialize()
+	Duel.Draw(1,1,REASON_RULE)
+end
+
 function Auxiliary.PreloadUds()
 	Duel.EnableGlobalFlag(GLOBALFLAG_DECK_REVERSE_CHECK)
 	local e2=Effect.GlobalEffect()
@@ -7,4 +11,14 @@ function Auxiliary.PreloadUds()
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetTargetRange(1,1)
 	Duel.RegisterEffect(e2,0)
+	-- one more draw
+	local e1=Effect.GlobalEffect()
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetCode(EVENT_ADJUST)
+	e1:SetOperation(function(e)
+		inititialize()
+		e:Reset()
+	end)
+	Duel.RegisterEffect(e1,0)
 end
