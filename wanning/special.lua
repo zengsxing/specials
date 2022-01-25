18eb786f2163310abc3d6b3ad919b61698770070
purerosefallen <78877@qq.com>
2022-01-25 22:36:29 +0800

    fix

--- wanning/special.lua
+++ wanning/special.lua
@@ -154,7 +154,7 @@ endPhaseSkill(69015963, function(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetValue(500)
-	e1:SetReset(RESET_EVENT+0x1fe0000)
+	e1:SetReset(RESET_EVENT+0xfe0000)
 	tc:RegisterEffect(e1,true)
 	Duel.SpecialSummonComplete()
 	end
