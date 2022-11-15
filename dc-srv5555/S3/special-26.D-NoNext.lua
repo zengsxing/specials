cbb2d02da2219f99e0cf6c0deee4a3d72991a404
Joy J <316351048@qq.com>
2022-11-15 17:08:00 +0800

    fix srv5555 script NoNext

--- dc-srv5555/S3/special-26.D-NoNext.lua
+++ dc-srv5555/S3/special-26.D-NoNext.lua
@@ -3,10 +3,12 @@
 --X的数量是原限制次数*3。
 
 CUNGUI = {}
+CUNGUI.index = 10002
 local scl = Effect.SetCountLimit
 Effect.SetCountLimit = function(e,count,code)
 	if not code then
-		code = EFFECT_COUNT_CODE_DUEL
+		code = EFFECT_COUNT_CODE_DUEL + CUNGUI.index
+		CUNGUI.index = CUNGUI.index + 1
 	else
 		code = code | EFFECT_COUNT_CODE_DUEL
 	end
