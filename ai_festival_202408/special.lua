67a07ddf8e80573b65402372b338c4570588bd6a
jwyxym <130654339+jwyxym@users.noreply.github.com>
2024-11-23 23:19:53 +0800

    修复因规则卡而导致“帝王”等卡不能使用的问题 (#63)
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * Update special-step3.lua
    
    * fix
    
    * 2337服12.06比赛所需
    
    * fix 新宇&记忆破坏者
    
    * Revert "fix 新宇&记忆破坏者"
    
    This reverts commit 76895c88f0779adfeb59b54f2fae4a75a37bd60e.
    
    * fix
    
    * fix
    
    * 补完发牌姬缺少的Duel.SpecialSummonComplete()
    
    * 更改规则卡到额外卡组表侧
    
    这个规则卡不会被作为cost，不能特殊召唤，不能加入手卡、送去墓地、送回卡组
    
    * 增加step1的每回合提示规则
    
    * 修复规则卡效果的注册位置
    
    * 修复MDPro3规则卡有bug的问题
    
    * 修不了，跑路.jpg
    
    * 修复因规则卡而导致“帝王”等卡不能使用的问题
    
    ---------
    
    Co-authored-by: jwyxym <>

--- ai_festival_202408/special.lua
+++ ai_festival_202408/special.lua
@@ -5,3 +5,13 @@ function Auxiliary.PreloadUds()
     Duel.LoadScript("special-step" .. tostring(rule_number) .. ".lua")
     CUNGUI.PreloadUds()
 end
+
+function Auxiliary.NoCardsInExtra(e)
+	return Auxiliary.GetExtra(e,e:GetHandlerPlayer()):GetCount()==0
+end
+
+function Auxiliary.GetExtra(e,tp)
+	return Duel.GetMatchingGroup(function (c)
+        return c:IsFacedown() or c:IsType(TYPE_PENDULUM)
+    end,tp,LOCATION_EXTRA,0,nil)
+end
