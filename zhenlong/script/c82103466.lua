0aaa70b4db25eaf612a1f588fef176de623b3898
Nanahira <78877@qq.com>
2024-12-26 16:07:20 +0800

    Update c82103466.lua

--- zhenlong/script/c82103466.lua
+++ zhenlong/script/c82103466.lua
@@ -1,6 +1,5 @@
 --蛇神 格
-local s,id,o=GetID()
-function s.initial_effect(c)
+function c82103466.initial_effect(c)
 	c:EnableReviveLimit()
 	--connot special summon
 	local e0=Effect.CreateEffect(c)
@@ -10,44 +9,116 @@ function s.initial_effect(c)
 	c:RegisterEffect(e0)
 	--special summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(82103466,0))
+	e1:SetDescription(aux.Stringid(m,0))
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_DESTROYED)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCondition(c82103466.spcon)
-	e1:SetCost(c82103466.spcost)
+	--e1:SetCost(c82103466.spcost)
 	e1:SetTarget(c82103466.sptg)
 	e1:SetOperation(c82103466.spop)
 	c:RegisterEffect(e1)
-	
-	local e00=Effect.CreateEffect(c)
-	e00:SetType(EFFECT_TYPE_FIELD)
-	e00:SetRange(LOCATION_MZONE)
-	e00:SetCode(EFFECT_CANNOT_LOSE_KOISHI)
-	e00:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e00:SetTargetRange(1,0)
-	e00:SetValue(1)
-	c:RegisterEffect(e00)
+	--
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_LEAVE_FIELD)
+	e2:SetOperation(c82103466.leaveop)
+	c:RegisterEffect(e2)
+	--LP remain 0
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_ADJUST)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(c82103466.condition)
+	e3:SetOperation(c82103466.operation)
+	c:RegisterEffect(e3)
+	--immune
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_IMMUNE_EFFECT)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetValue(c82103466.efilter)
+	c:RegisterEffect(e3)
+	--attack cost
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_ATTACK_COST)
+	e4:SetCost(c82103466.atcost)
+	e4:SetOperation(c82103466.atop)
+	c:RegisterEffect(e4)
+    --cannot lose
+	local ex1=Effect.CreateEffect(c)
+	ex1:SetType(EFFECT_TYPE_FIELD)
+	ex1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	ex1:SetRange(LOCATION_MZONE)
+	ex1:SetCode(EFFECT_CANNOT_LOSE_KOISHI)
+	ex1:SetTargetRange(1,0)
+	ex1:SetValue(1)
+	local ex2=Effect.CreateEffect(c)
+	ex2:SetType(EFFECT_TYPE_FIELD)
+	ex2:SetCode(0x10000000+82103466)
+	ex2:SetRange(LOCATION_MZONE)
+	ex2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	ex2:SetTargetRange(1,0)
+	c:RegisterEffect(ex2)
+	c:RegisterEffect(ex1)
+	if not c82103466.lose_check then
+		c82103466.lose_check=true
+		local fuc_win=Duel.Win
+		function Duel.Win(player,reason)
+			if Duel.GetFlagEffect(1-player,82103466)~=0 then
+				return false
+			else
+				return fuc_win(player,reason)
+			end
+		end
+	end
 end
-
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp)
+function c82103466.cfilter(c,tp)
+	return c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE)
+		and ((c:IsReason(REASON_BATTLE) and Duel.GetAttacker():IsControler(1-tp))
+		or (c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==1-tp))
 end
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c82103466.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c82103466.cfilter,1,nil,tp)
+end
+function c82103466.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.PayLPCost(tp,math.floor(Duel.GetLP(tp)/2))
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c82103466.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,true,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
+function c82103466.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	if Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)~=0 then
 		c:CompleteProcedure()
+		Duel.BreakEffect()
+		Duel.SetLP(tp,0)
 	end
+end
+function c82103466.leaveop(e,tp,eg,ep,ev,re,r,rp)
+	local WIN_REASON_RELAY_SOUL=0x1a
+	Duel.Win(1-tp,WIN_REASON_RELAY_SOUL)
+end
+function c82103466.condition(e,tp)
+	return Duel.GetLP(tp)>0
+end
+function c82103466.operation(e,tp)
+	Duel.SetLP(tp,0)
+end
+function c82103466.efilter(e,te)
+	return te:GetOwner()~=e:GetOwner()
+end
+function c82103466.atcost(e,c,tp)
+	return Duel.IsPlayerCanDiscardDeckAsCost(tp,10)
+end
+function c82103466.atop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.DiscardDeck(tp,10,REASON_COST)
 end
