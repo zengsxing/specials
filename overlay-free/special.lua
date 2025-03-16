455ba89bbf594e792c8c6afc5e168b795b70271e
今晚有宵夜吗 <130654339+jwyxym@users.noreply.github.com>
2025-03-16 17:12:06 +0800

    尼亚要的仪式规则 (#105)
    * 仪式用
    
    * 更改位置（铸造屎山）

--- overlay-free/special.lua
+++ overlay-free/special.lua
@@ -32,4 +32,69 @@ function aux.PreloadUds()
   e2:SetLabelObject(e1)
   e2:SetValue(1)
   Duel.RegisterEffect(e2,0)
+
+  RITUAL.Register()
 end
+
+RITUAL = {
+  CHK_LIST = {
+    [0] = {
+      [TYPE_MONSTER] = 0,
+      [TYPE_SPELL] = 0
+    },
+    [1] = {
+      [TYPE_MONSTER] = 0,
+      [TYPE_SPELL] = 0
+    }
+  },
+  CHK_FUNC = function (tp)
+    return RITUAL.CHK_LIST[tp][TYPE_MONSTER] >= 2 and RITUAL.CHK_LIST[tp][TYPE_SPELL] >= 2 and RITUAL.CHK_LIST[tp][TYPE_MONSTER] + RITUAL.CHK_LIST[tp][TYPE_SPELL] >= 5
+  end,
+  SP = function (c)
+    local e = Effect.CreateEffect(c)
+    e:SetDescription(1075)
+    e:SetType(EFFECT_TYPE_FIELD)
+    e:SetCode(EFFECT_SPSUMMON_PROC)
+    e:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e:SetRange(LOCATION_HAND)
+    e:SetCountLimit(1,66666666 + EFFECT_COUNT_CODE_OATH)
+    e:SetCondition(function (this_e)
+      local this_c = this_e:GetHandler()
+      if not RITUAL.CHK_FUNC(this_e:GetHandlerPlayer()) then return false end
+      return this_c:IsType(TYPE_RITUAL) and this_c:IsType(TYPE_MONSTER) and this_c:IsRace(RACE_WARRIOR + RACE_FAIRY)
+    end)
+    e:SetTarget(function (this_e,tp,eg,ep,ev,re,r,rp,chk,this_c)
+      local lp = this_c:GetLevel() * 500
+      if Duel.CheckLPCost(tp, lp) then
+        e:SetLabel(lp)
+        return true
+      else return false end
+    end)
+    e:SetOperation(function (this_e,tp,eg,ep,ev,re,r,rp,this_c)
+      Duel.PayLPCost(tp,e:GetLabel())
+    end)
+    c:RegisterEffect(e)
+  end,
+  Register = function ()
+    local e = Effect.GlobalEffect()
+    e:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+    e:SetCode(EVENT_ADJUST)
+    e:SetOperation(function (this_e)
+      for tp = 0, 1, 1 do
+        for c in aux.Next(Duel.GetFieldGroup(tp, 0xff, 0)) do
+          RITUAL.SP(c)
+          if c:IsType(TYPE_RITUAL) then
+            if c:IsType(TYPE_MONSTER) then
+              RITUAL.CHK_LIST[tp][TYPE_MONSTER] = RITUAL.CHK_LIST[tp][TYPE_MONSTER] + 1
+            elseif c:IsType(TYPE_SPELL) then
+              RITUAL.CHK_LIST[tp][TYPE_SPELL] = RITUAL.CHK_LIST[tp][TYPE_SPELL] + 1
+            end
+          end
+        end
+      end
+      this_e:Reset()
+    end)
+    Duel.RegisterEffect(e,0)
+  end
+}
